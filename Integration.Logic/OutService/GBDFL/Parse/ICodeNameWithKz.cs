68df1e6fb953d2d8dffb57c6d4cdcc907a174eb5
Nursultan Kassan <kassan.nk@outlook.com>
2023-03-15 22:58:00 +0600

    Delete Integration.Logic/OutService directory

--- Integration.Logic/OutService/GBDFL/Parse/ICodeNameWithKz.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Agro.Integration.Logic.OutService.GBDFL.Parse
-{
-    public interface ICodeNameWithKz
-    {
-        string Code { get; set; }
-        string Name { get; set; }
-        string NameKz { get; set; }
-    }
-}
